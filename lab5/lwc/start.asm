92809e3dd139909ae47a9a2016aad84ee3205c25
Chen Israeli <chenisraeli1987@gmail.com>
2012-12-02 17:00:01 +0200

    added lab5

--- lab5/lwc/start.asm
+++ lab5/lwc/start.asm
@@ -12,7 +12,7 @@ _start:
         mov ecx,esp
 	add ecx,4
 	push ecx
-        push [esp+4]
+        push DWORD[esp+4]
 	call	main
         mov     ebx,eax
 	mov	eax,1
@@ -50,19 +50,23 @@ close:
 
 
 strlen:
-       mov eax,DWORD [esp+4]
-       movzx	eax, BYTE [eax]
-       mov [esp-4],0
-       sub esp. 4
+       push ebp
+       mov ebp,esp
+       sub esp, 4
+       mov eax,DWORD [ebp+8]
+       movzx eax, BYTE [eax]
+       mov DWORD[ebp-4],0
 
-.TEST: test al,al
+.TEST: 
+       test al,al
        je .END
-       add DWORD [esp+4],1
+       add DWORD [ebp+8],1
        add DWORD [esp-4],1
-       mov eax,DWORD [esp+4]
-       movzx	eax, BYTE [eax]
+       mov eax,DWORD [ebp+8]
+       movzx eax, BYTE [eax]
        jmp .TEST
 
 .END:  mov eax, DWORD [esp-4]
-       add esp,4
+       mov esp, ebp
+       pop ebp
        ret
